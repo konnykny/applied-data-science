0eead96d078e86360896f81ca643ff31de8cf617
Richard Kraus <richi.kraus149@gmail.com>
2025-10-31 09:55:32 +0100

    main cleanup

--- main.py
+++ main.py
@@ -1,70 +1,53 @@
 #%%
 import os
 from sklearn.pipeline import Pipeline
-
 from preprocessing import Preprocessing
 from exploration import Exploration
 from features import FeatureExtraction, SaveORLoad
 from modeling import ModelTraining
 
 
-def build_pipeline(data_dir_or_paths, ffill_amt=3, sampling_interval=1, drop_na=False,
-                   load_pickle_instead=None, save_pickle=True,
-                   N_past_values=72, nan_report=True, plot_data="All",
-                   target_column=None, test_size=0.2, random_state=42, plot_results=True, drop_energy_details=True):
+def build_pipeline(data_dir_or_paths, target_column=None):
+
     pipe = Pipeline(steps=[
         ("#1 Preprocessing", Preprocessing(
-            data_path=data_dir_or_paths,
-            ffill_amt=ffill_amt,
-            sampling_interval=sampling_interval,
-            drop_na=drop_na,
-            load_pickle_instead=load_pickle_instead,
-            save_pickle=save_pickle,
-            drop_energy_details=drop_energy_details,
+            data_path = data_dir_or_paths,
+            ffill_amt = 3,
+            sampling_interval = 1,
+            drop_na = False,
+            load_pickle_instead = None, #f'{data_dir}\preprocessed_dataframe.pkl',
+            save_pickle = True,
+            drop_energy_details = False,
         )),
         # ("#2 Explore", Exploration(
-        #     nan_report=nan_report,
-        #     plot_data=plot_data
+        #     nan_report = True,
+        #     plot_data = None, # "All" | "weather" | "energy" | None | "seaborn"
         # )),
         ("#3 Calculate features", FeatureExtraction(
             target_column, 
-            N_past_values=N_past_values, 
-            N_future_values=24, 
-            weather_columns=['temp', 'temp_max'], 
+            N_past_values= [24, 24*7],
+            N_future_values= 24,
+            weather_columns= ['temp', 'wind_speed', 'clouds_all', 'rain_1h'],
+            generated_locations = ['Madrid'], # choose city locations for feature generation (zenith, azimuth, etc.)
         )),
         ("save_features", SaveORLoad(mode='save')),
         #("save_features", SaveORLoad(mode='load')),
         ("#4 Train Models", ModelTraining(
-            target_column=target_column,
-            test_size=test_size,
-            random_state=random_state,
-            plot_results=plot_results
+            target_column = 'renewable_generation_ratio',
+            test_size=0.2,
+            random_state=42,
+            plot_results=True
         ))
     ])
     return pipe
 
 
 if __name__ == "__main__":
-    data_dir = './data'#os.environ.get("DATA_DIR", ".")
-
-    target_col = 'renewable_generation_ratio'
 
     pipeline = build_pipeline(
-        data_dir_or_paths=data_dir,
-        ffill_amt=3,
-        drop_na=False, #drop rows with any NaN after ffill
-        load_pickle_instead=f'{data_dir}\preprocessed_dataframe.pkl',
-        save_pickle=True,
-        N_past_values=[24, 24*7],
-        nan_report=True,
-        plot_data="energy", # "All" | "weather" | "energy" | None | "seaborn"
-        target_column=target_col,
-        test_size=0.2,
-        random_state=42,
-        plot_results=True,
-        drop_energy_details=False
+        data_dir_or_paths='./data', #os.environ.get("DATA_DIR", "."),
+        target_column='renewable_generation_ratio'
     )
-
     output = pipeline.fit_transform(None)
-    pass
+    pass # for breakpoint
     print("[main] Pipeline completed.")
